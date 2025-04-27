44b2d612992ade08f739b05f4d9c8c8fc871720a
Joaqim Planstedt <mail@joaqim.xyz>
2025-04-27 15:50:41 +0200

    feat(mods): Add `Dwarfs and Giants`

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
           # Run `nix flake check ./?dir=dev&submodules=1` will give you the correct hash to assign below.
           # When you've set the hash, the next build will return with a `/nix/store` location
           # of the entry of the modpack, which will also be symlinked into `./result/`.
-          modrinth-pack-hash = "sha256-FFwoPwHdZ1wCbSYuhOyEaYZFb55g6tqkfMkBRE7gJDQ=";
+          modrinth-pack-hash = "sha256-66QNKj1QswMAVQy8H5u3FryZBWIhbP7WroW3O1nYUdY=";
         in {
           modrinth-pack = pkgs.callPackage ./nix/packwiz-modrinth.nix {
             src = self;
