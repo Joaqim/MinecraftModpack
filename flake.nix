b6154150e9e05b351918d225d9c6665414fd3c3b
Joaqim Planstedt <mail@joaqim.xyz>
2025-07-05 13:06:38 +0200

    revert(mods): Remove `More Chests Variants`

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
           # Run `nix flake check ./?dir=dev&submodules=1` will give you the correct hash to assign below.
           # When you've set the hash, the next build will return with a `/nix/store` location
           # of the entry of the modpack, which will also be symlinked into `./result/`.
-          modrinth-pack-hash = "sha256-vWmkSnq3JdOT0dqVo42PtL+iyJN2VOg2nBUsj+k6vsY=";
+          modrinth-pack-hash = "sha256-bqzKa8FijmxJ/IxL+5PVUxOLicSunAIzGLV8w+udgp4=";
         in {
           modrinth-pack = pkgs.callPackage ./nix/packwiz-modrinth.nix {
             src = self;
